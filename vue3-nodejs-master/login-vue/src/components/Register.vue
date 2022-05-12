6c3e22ecdfe37f90763fe8e282c21644d9fc608f
zp <763360229@qq.com>
2022-05-12 10:38:54 +0800

    12

--- vue3-nodejs-master/login-vue/src/components/Register.vue
+++ vue3-nodejs-master/login-vue/src/components/Register.vue
@@ -2,13 +2,33 @@
   <div id="register">
     <label class="lab">
       <span class="demo-input-label">Input Your Email:</span>
-      <el-input v-model="email" placeholder="Please input" clearable @blur="checkEmail"/>
-      <div v-show ="isEmail" class="check"><span>邮箱格式不符合规则</span></div>
+      <el-input
+        v-model="email"
+        placeholder="Please input"
+        clearable
+        @blur="checkEmail"
+      />
+      <div v-show="isEmail" class="check"><span>邮箱格式不符合规则</span></div>
     </label>
     <label class="lab">
       <span class="demo-input-label">Input Your Password:</span>
-      <el-input v-model="psw" placeholder="Please input password" show-password @blur="checkPsw" @keyup.enter="register"/>
-      <div v-show ="isPsw" class="check"><span>密码不符合规则</span></div>
+      <el-input
+        v-model="psw"
+        placeholder="Please input password"
+        show-password
+        @blur="checkPsw"
+        @keyup.enter="register"
+      />
+      <div v-show="isPsw" class="check"><span>密码不符合规则</span></div>
+    </label><br>
+        <label class="lab">
+      <span class="demo-input-label">Input Your name:</span>
+      <el-input
+        v-model="Name"
+        placeholder="Please input"
+        clearable
+      />
+      <!-- <div v-show="isEmail" class="check"><span>邮箱格式不符合规则</span></div> -->
     </label>
     <br />
     <div id="btn">
@@ -27,9 +47,10 @@ export default {
   setup() {
     const email = ref(""); // 绑定 email
     const psw = ref(""); // 绑定 密码
+    const Name = ref("")
     let isEmail = ref(false);
     let isPsw = ref(false);
-
+    console.log(email);
     const router = useRouter();
 
     // 检查 email 格式
@@ -38,14 +59,14 @@ export default {
       if (!emailReg.test(email.value) && email.value.length) {
         isEmail.value = true;
       } else isEmail.value = false;
-    }
+    };
 
     // 检查 password 格式
     const checkPsw = () => {
       if (psw.value.length > 0 && psw.value.length < 6) {
         isPsw.value = true;
       } else isPsw.value = false;
-    }
+    };
 
     const register = () => {
       if (!email.value || !psw.value) {
@@ -60,7 +81,7 @@ export default {
       axios({
         method: "post",
         url: "http://127.0.0.1:3000/api/user/add",
-        data: { email: email.value, password: psw.value },
+        data: { email: email.value, password: psw.value ,name:Name.value,qq:'1235456'},
       }).then((res) => {
         if (res.data.status === "3") {
           router.push("/home/login");
@@ -81,11 +102,13 @@ export default {
     return {
       email,
       psw,
+        Name,
       register,
       checkEmail,
       isEmail,
       checkPsw,
-      isPsw
+      isPsw,
+    
     };
   },
 };
--- vue3-nodejs-master/service/api/router.js
+++ vue3-nodejs-master/service/api/router.js
@@ -5,8 +5,8 @@ const token = require('./token.js');
 
 const connection = mysql.createConnection({
     host: '127.0.0.1',
-    user: 'dev',
-    password: '@HautZhj6248',
+    user: 'root',
+    password: '123456',
     database: 'login'
 })
 connection.connect();
@@ -14,12 +14,12 @@ connection.connect();
 // 登录模块
 router.post('/login', (req, res) => {
     const user = req.body;
-    // console.log(req.body);
+    console.log(req.body);
     // 定义查询 sql
-    const sel_email_sql =  `select * from user where email = '${user.email}'`;
+    const sel_email_sql = `select * from user where email = '${user.email}'`;
     connection.query(sel_email_sql, (err, results) => {
         if (err) throw err;
-        // console.log(results);
+        console.log(results);
         if (results[0] === undefined) {
             res.json({
                 status: '-1',
@@ -46,13 +46,12 @@ router.post('/login', (req, res) => {
 // 注册模块
 router.post('/add', (req, res) => {
     const register = req.body;
-    // console.log(register);
+    console.log(register);
     // 查询 sql 防止一个邮箱重复注册
     const sel_sql = `select * from user where email = '${register.email}'`;
 
     // 添加 sql
-    const add_sql = 'insert into user(email, password) values (?, ?)';
-
+    const add_sql = 'insert into user(email, password,userName,userQQ) values (?, ?,?,?)';
     connection.query(sel_sql, (err, results) => {
         if (err) throw err;
         if (results.length !== 0 && register.email === results[0].email) {
@@ -61,7 +60,7 @@ router.post('/add', (req, res) => {
                 message: '邮箱已被注册，注册失败！'
             })
         } else {
-            connection.query(add_sql, [register.email, register.password], (err, results) => {
+            connection.query(add_sql, [register.email, register.password, register.name, register.qq], (err, results) => {
                 if (err) throw err;
                 else {
                     res.send({
